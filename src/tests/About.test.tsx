cfd102d67abb2de5f2f6a67895410dcabcbc06a1
Roman <americano.creem@gmail.com>
2023-09-16 19:56:14 +0500

    FIX: About page test

--- src/tests/About.test.tsx
+++ src/tests/About.test.tsx
@@ -1,9 +1,19 @@
-import { render, screen } from '@testing-library/react'
-import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react';
+import renderer from 'react-test-renderer';
+import '@testing-library/jest-dom';
 import About from '../Components/About/About';
 
-test('Correct render of AboutPage', () => {
-    render(<About />);
-    const linkElem = screen.getByText('Tech stack');
-    expect(linkElem).toBeInTheDocument();
-});
+describe('About page', () => {
+    test('About Page text render', () => {
+        render(<About />);
+        const linkElem = screen.getByText('Tech stack');
+        expect(linkElem).toBeInTheDocument();
+    });
+
+    test('Number of links', () => {
+        render(<About />);
+        const linkElem = screen.getByRole('a');
+        expect(linkElem).toBeInTheDocument();
+    })
+})
+
