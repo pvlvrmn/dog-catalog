1c130966d0c11d753ec02903cb767c041502cbac
Roman <americano.creem@gmail.com>
2023-09-17 11:43:17 +0500

    FIX: About links (jest)

--- src/Components/About/About.tsx
+++ src/Components/About/About.tsx
@@ -14,6 +14,7 @@ function About() {
           <li><a rel="noreferrer" href="https://react.dev/" target="_blank">React</a></li>
           <li><a rel="noreferrer" href="https://reactrouter.com/en/main" target="_blank">React Router</a></li>
           <li><a rel="noreferrer" href="https://www.typescriptlang.org/" target="_blank">Typescript</a></li>
+          <li><a rel="noreferrer" href="https://jestjs.io/" target="_blank">Jest</a></li>
           <li><a rel="noreferrer" href="https://vitejs.dev/" target="_blank">Vite</a></li>
           <li><a rel="noreferrer" href="https://eslint.org/" target="_blank">ESLint</a></li>
           <li><a rel="noreferrer" href="https://www.npmjs.com/" target="_blank">npm</a></li>
